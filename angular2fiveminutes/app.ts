b30f60c20b1590220bcac442437007f11c522509
John Papa <john@johnpapa.net>
2015-08-08 08:26:20 -0400

    init angular2 in 5 minutes

---
+++ angular2fiveminutes/app.ts
@@ -0,0 +1,17 @@
+import {Component, View, bootstrap} from 'angular2/angular2';
+
+@Component({
+  selector: 'my-app'
+})
+@View({
+  template: '<h1>Hello {{ name }}</h1>'
+})
+// Component controller
+class MyAppComponent {
+  name: string;
+  constructor() {
+    this.name = 'Dude';
+  }
+}
+
+bootstrap(MyAppComponent);
